d2cbdfc51d9845230d01994ff58fefa41ed86c0f
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-04-24 20:11:51 +0200

    add psr4 autoload path for new testCase class

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,6 @@
 <?php
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->setPsr4('Saxulum\Tests\DoctrineMongoDbOdm\\', __DIR__);
+$loader->setPsr4('Saxulum\Tests\\', __DIR__);
 
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
