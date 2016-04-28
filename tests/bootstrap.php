d9471842e4b4b02e557a206f75793e4b873539ca
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-04-28 11:40:03 +0200

    fix unit test autoload problem

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,8 @@
 <?php
 
 $loader = require __DIR__.'/../vendor/autoload.php';
-$loader->setPsr4('Saxulum\\Tests\\DoctrineMongoDbOdm\\', __DIR__);
 $loader->setPsr4('Saxulum\\Tests\\', __DIR__);
+$loader->setPsr4('Saxulum\\Tests\\DoctrineMongoDbOdm\\', __DIR__);
+$loader->setPsr4('Saxulum\\DoctrineMongoDb\\', sprintf('%s/../vendor/df/doctrine-mongodb-provider/src/', __DIR__));
 
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
